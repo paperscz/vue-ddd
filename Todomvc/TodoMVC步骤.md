f970137dacd0cdb53972cc9c80cd0fef2b0b091c
DW1AU17 <438981082@qq.com>
2019-03-02 20:34:24 +0800

    vue项目的前两天代码,练习和todomcv练习

---
+++ Todomvc/TodoMVC步骤.md
@@ -0,0 +1,54 @@
+## 一. 从github上引入todo项目的静态页面
+    1. (如果没有node_modules文件)安装对应模板文件(css文件..) ==> 查看packgae
+    2. npm i 安装 
+
+## 二. 分析结构,划分区域
+    1. 分析一个标签的功能需要哪些东西
+        1.1 选中状态 , checkked
+        1.2 标签名 , name
+        1.3 删除时 , id
+
+    2. 新建数据
+        data: {
+			list: [
+				{ id: 1, status: false, name: "屠杀环节" },
+				{ id: 2, status: false, name: "旅行环节" },
+				{ id: 3, status: true, name: "吸金环节" },
+			]
+		}
+
+## 三. 配置Vue
+    1. 安装vue
+    2. 引入vue.js (app.js之前,因为app.js基于vue.js)
+    3. 实例化对象vm
+
+## 四. 渲染数据
+    1. 把数据渲染到页面上(v-for)
+    2. 解决划线, 选中的问题
+
+## 五. 删除单条数据(三种方法)
+
+1. 通过数组的截取方法 splice
+    1.1 注册点击事件(@click),通过下标 (传参 index)
+    2.1 数组splice(id,1)截取来着一项
+
+2. 通过遍历过滤(filter)出匹配的(不为当前id的), 通过id
+
+3. 通过id --> 找到下标(数组遍历的findIndex方法) --> splice
+
+
+## 六. 添加数据
+    1. 要获取input的值,(双向绑定 --> V层新建一个数据inputValue)
+    2. 注册键盘事件 keyup,点enter添加
+    3. 获取这个数据, 新建id(数组最后一项的id+1,当数组为空时设置默认值) ,和默认状态 --> 保存到数组list中
+       (不能为空,在清空val值)
+
+## 七. 修改单项
+
+    1. 注册双击事件 dblclick 
+    2. (设置默认数值-1)  --> 判断item.id == 默认值
+    3. 改变editing的状态(改变默认值为当前的id值), 打开编辑器   
+    4. 注册键盘事件keyup,恢复状态的默认值为 -1 ,关闭编辑器
+
+## 八. 底部的显示和隐藏( v-if / v-show )
+    1. 当list.length > 0 时,显示
---
+++ Todomvc/todomvc-app-template/js/app.js
@@ -0,0 +1,69 @@
+(function (window) {
+	'use strict';
+
+	// Your starting point. Enjoy the ride!
+	const vm = new Vue({
+		el: "#app",
+		data: {
+			list: [
+				{ id: 1, name: "旅游环节", status: false },
+				{ id: 2, name: "屠杀环节", status: false },
+				{ id: 3, name: "吸金环节", status: true }
+			],
+			inputValue: '',
+			editId: -1,
+			focusStatus: false,
+		},
+		methods: {
+			// 删除数据
+			// 方法1: 通过下标的方法
+			// destroyItem(index) {
+			// 	this.list.splice(index, 1)
+			// }
+			// 方法2: 通过过滤 
+			// destroyItem(id) {
+			// 	this.list = this.list.filter( item => item.id != id )
+			// }
+			// 方法3: 通过id -> index ->splice
+			destroyItem(id) {
+				let index = this.list.findIndex(item => item.id == id)
+				this.list.splice(index, 1)
+			},
+			// 保存数据
+			saveDate() {
+				if (this.inputValue.trim() == '') {
+					return
+				}
+				let id = this.list.length == 0 ? 1 : this.list[this.list.length - 1].id + 1
+				this.list.push({
+					id,   //id:id  --> 可简写
+					name: this.inputValue,
+					status: false
+				})
+				this.inputValue = ''
+			},
+			//编辑数据
+			editData(id) {
+				this.editId = id
+			},
+			//关闭编辑器
+			closeEdit() {
+				this.editId = -1
+			}
+		},
+		computed:{
+			showStatus() {
+				return this.list.length > 0
+			}
+		},
+		directives: {
+			'todo-focus': function (el) {
+				el.focus();
+			}
+		}
+	})
+
+
+
+
+})(window);
